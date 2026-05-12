3d7b51974370fa8f6562923b957e1f71eff59b92
Kevin P. Erkelenz <kevinerkelenz@gmail.com>
2026-05-11 22:24:14 -0700

    Build register page with form validation and API connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<h1>Login Page</h1>} />
-        <Route path="/register" element={<h1>Register Page</h1>} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<h1>Dashboard</h1>} />
         <Route path="/profile" element={<h1>Profile</h1>} />
       </Routes>
